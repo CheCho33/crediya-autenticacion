e9a6e2d2437debf2dcd072294c163189b3cba683
Sergio <sergio_andrestb@yahoo.es>
2025-08-25 13:24:24 -0500

    estructura el proyecto.

---
+++ domain/model/src/main/java/co/com/crediya/autenticacion/model/rol/gateways/RolRepository.java
@@ -0,0 +1,4 @@
+package co.com.crediya.autenticacion.model.rol.gateways;
+
+public interface RolRepository {
+}
---
+++ domain/usecase/src/main/java/co/com/crediya/autenticacion/usecase/usuario/UsuarioUseCase.java
@@ -0,0 +1,6 @@
+package co.com.crediya.autenticacion.usecase.usuario;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class UsuarioUseCase {
+}
